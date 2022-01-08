7aadd9393eabe0e4d2da99f4ce423523b6ce84a2
James Troughton <jamesstroughton@gmail.com>
2022-01-08 14:30:06 +1300

    ➕ Imports

--- components/index.js
+++ components/index.js
@@ -1,3 +1,8 @@
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
 export { default as Hero } from './Hero';
+export { default as Featured } from './Featured';
+export { default as About } from './About';
+export { default as SectionHeader } from './SectionHeader';
+export { default as Gallery } from './Gallery';
+export { default as Footer } from './Footer';
