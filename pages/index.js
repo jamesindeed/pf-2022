df1350a83a742757ffa5cf5c5afe355b567cbfbd
James Troughton <jamesstroughton@gmail.com>
2022-01-08 14:28:38 +1300

    ✨ Site Home

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,46 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
+import { About, Featured, Footer, Gallery, Hero } from '../components';
 
 export default function Home() {
+  const [preloader, setPreloader] = useState(true);
+
+  const handlePreloader = () => {
+    setPreloader(false);
+  };
+
   return (
     <>
       <Head>
-        <title>JT</title>
+        <title>James Troughton</title>
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
         />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Syncopate:wght@400;700&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap'
-        />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap'
-        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='container px-10 mx-auto mb-8 max-w-7xl'>
-        {/* Components */}
-      </div>
+
+      {preloader ? (
+        <div
+          onClick={handlePreloader}
+          className='transition-all ease-in-out duration-250 absolute-class flex items-center justify-center flex-col bg-[#181717] text-[#f5f0ec]'
+        >
+          <h1 className='uppercase font-semibold text-[2vw] font-["Baj_Jamjuree"]'>
+            James Troughton
+          </h1>
+          <h2 className='uppercase italic mt-2 text-[2vw] font-["Baj_Jamjuree"]'>
+            Auckland, New Zealand
+          </h2>
+        </div>
+      ) : (
+        <div className='px-[5vw]' id='main-container' data-scroll-container>
+          <Hero />
+          <Featured />
+          <About />
+          <Gallery />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
-``;
