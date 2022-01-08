f09fa7f99db8b73bc972eba5725199c1c360068b
James Troughton <jamesstroughton@gmail.com>
2022-01-08 14:30:30 +1300

    ✨ Section Header Component

---
+++ components/SectionHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SectionHeader({ title }) {
+  return (
+    <h6 className='uppercase font-semibold font-[Syncopate] text-[#626262] mb-[50px]'>
+      {title}
+    </h6>
+  );
+}
