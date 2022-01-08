bd6bf0ccb5068c02c0a1cca2a58f4df155248677
James Troughton <jamesstroughton@gmail.com>
2022-01-08 14:29:54 +1300

    💄 Minor Styles

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,10 +3,10 @@ import React, { useEffect } from 'react';
 const Hero = () => {
   return (
     <section
-      className='relative pb-[100px] left-0 font-semibold tracking-wide text-gray-700 uppercase top-32'
+      className='relative pb-[100px] left-0 font-semibold tracking-wide text-[#252422] uppercase top-32'
       data-scroll-section
     >
-      <ul className='absolute sm:top-0 -top-28 font-[Syncopate] text-red-800 space-y-1'>
+      <ul className='absolute sm:top-0 -top-28 font-[Syncopate] text-[#780000] space-y-1'>
         <li>
           <a href='#intro'>Intro</a>
         </li>
@@ -19,7 +19,7 @@ const Hero = () => {
       </ul>
       <h1
         id='header-text'
-        className='pb-[100px] font-["Bai_Jamjuree"] text-center uppercase text-[14vw] 4xl:text-[10vw] leading-none font-bold'
+        className='pb-[100px] font-["Bodoni_Moda"] tracking-tighter text-center uppercase text-[14vw] 4xl:text-[10vw] leading-none font-bold'
       >
         James Troughton
       </h1>
