fe801698d3ee65d3ca77182e2b386857d4a807bb
James Troughton <jamesstroughton@gmail.com>
2022-01-08 14:57:34 +1300

    ✨ Preloader Timer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Head from 'next/head';
 import { About, Featured, Footer, Gallery, Hero } from '../components';
 
 export default function Home() {
   const [preloader, setPreloader] = useState(true);
+
+  const [timer, setTimer] = useState(3);
+
+  const id = useRef(null);
+
+  const clear = () => {
+    window.clearInterval(id.current);
+    setPreloader(false);
+  };
+
+  useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer((timer) => timer - 1);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (timer === 0) {
+      clear();
+    }
+  }, [timer]);
 
   const handlePreloader = () => {
     setPreloader(false);
@@ -25,6 +46,9 @@ export default function Home() {
           onClick={handlePreloader}
           className='transition-all ease-in-out duration-250 absolute-class flex items-center justify-center flex-col bg-[#181717] text-[#f5f0ec]'
         >
+          <h2 className='uppercase italic mt-2 text-[2vw] font-["Baj_Jamjuree"]'>
+            {timer}
+          </h2>
           <h1 className='uppercase font-semibold text-[2vw] font-["Baj_Jamjuree"]'>
             James Troughton
           </h1>
