f64a0ad9975a8f001678e11f3942823ef88c456e
James Troughton <jamesstroughton@gmail.com>
2022-01-08 14:29:13 +1300

    🎨  Deleted some text

--- components/About.jsx
+++ components/About.jsx
@@ -7,13 +7,9 @@ const About = () => {
       <SectionHeader title='about' />
       <p className='text-[4.5rem] leading-[1.12]' id='headline'>
         I am a software engineer with skills in both front end and back end
-        development of websites and web applications. I have always been
-        interested in design and technology and I have delved into this career
-        path because of that. As the next step in my career I am looking to work
-        with a small team of passionate people to create and design outstanding
-        products for our clients. In doing this I am hoping to progress and grow
-        as a developer and continue to push the boundaries of what I can
-        achieve.
+        development of websites and web applications. As the next step in my
+        career I am looking to work with a small team of passionate people to
+        create and design outstanding products for our clients.
       </p>
     </section>
   );
