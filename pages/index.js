55443518ea5d458b3a36f74355d605cbf8b925db
James Troughton <jamesstroughton@gmail.com>
2022-01-11 11:51:31 +1300

    ✨ Smooth Scroll

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Head from 'next/head';
-import { About, Featured, Footer, Gallery, Hero } from '../components';
+import { Header, About, Featured, Footer, Gallery, Hero } from '../components';
+import useLocoScroll from '../hooks/useLocoScroll';
 
 export default function Home() {
   const [preloader, setPreloader] = useState(true);
 
+  useLocoScroll(!preloader);
   const [timer, setTimer] = useState(3);
 
   const id = useRef(null);
@@ -38,26 +40,26 @@ export default function Home() {
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
         />
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       {preloader ? (
         <div
           onClick={handlePreloader}
-          className='transition-all ease-in-out duration-250 absolute-class flex items-center justify-center flex-col bg-[#181717] text-[#f5f0ec]'
+          className='transition ease-in-out duration-250 absolute-class flex items-center justify-center flex-col bg-[#181717] text-[#f5f0ec]'
         >
-          <h2 className='uppercase italic mt-2 text-[2vw] font-["Baj_Jamjuree"]'>
+          <h2 className='uppercase italic mt-2 text-[3.5vw] md:text-[2.5vw] font-["Baj_Jamjuree"]'>
             {timer}
           </h2>
-          <h1 className='uppercase font-semibold text-[2vw] font-["Baj_Jamjuree"]'>
+          <h1 className='uppercase font-semibold text-[3.5vw] md:text-[2.5vw] font-["Baj_Jamjuree"]'>
             James Troughton
           </h1>
-          <h2 className='uppercase italic mt-2 text-[2vw] font-["Baj_Jamjuree"]'>
+          <h2 className='uppercase italic mt-2 text-[3.5vw] md:text-[2.5vw] font-["Baj_Jamjuree"]'>
             Auckland, New Zealand
           </h2>
         </div>
       ) : (
         <div className='px-[5vw]' id='main-container' data-scroll-container>
+          <Header />
           <Hero />
           <Featured />
           <About />
