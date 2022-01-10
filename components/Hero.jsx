8b0771b9896012ec7f8e1586e4528733972c721e
James Troughton <jamesstroughton@gmail.com>
2022-01-11 11:49:58 +1300

    💫Added animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect } from 'react';
+import { gsap } from 'gsap/dist/gsap';
 
 const Hero = () => {
+  useEffect(() => {
+    gsap.to('.header-text', {
+      duration: 1,
+      y: 0,
+      opacity: 1,
+      stagger: 0.1,
+      ease: 'power2',
+    });
+  }, []);
+
   return (
     <section
       className='relative pb-[100px] left-0 font-semibold tracking-wide text-[#252422] uppercase top-32'
@@ -17,12 +28,28 @@ const Hero = () => {
           <a href='#projects'>Projects</a>
         </li>
       </ul>
-      <h1
+      <div
         id='header-text'
-        className='pb-[100px] font-["Bodoni_Moda"] tracking-tighter text-center uppercase text-[14vw] 4xl:text-[10vw] leading-none font-bold'
+        className='header-text font-["Bodoni_Moda"] tracking-tighter text-center uppercase text-[14vw] 4xl:text-[13vw] leading-none font-bold'
       >
-        James Troughton
-      </h1>
+        <h1
+          id='header-text'
+          className='header-text font-["Bodoni_Moda"] tracking-tighter text-center uppercase text-[14vw] 4xl:text-[13vw] leading-none font-bold'
+        >
+          James
+        </h1>
+      </div>
+      <div
+        id='header-text-parent'
+        className='header-text pb-[100px] font-["Bodoni_Moda"] tracking-tighter text-center uppercase text-[14vw] 4xl:text-[13vw] leading-none font-bold'
+      >
+        <h1
+          id='header-text-parent'
+          className='header-text pb-[100px] font-["Bodoni_Moda"] tracking-tighter text-center uppercase text-[14vw] 4xl:text-[13vw] leading-none font-bold'
+        >
+          Troughton
+        </h1>
+      </div>
     </section>
   );
 };
