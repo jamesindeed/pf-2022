120d20001e6acbc05debc9da9d266ae4dc4653c8
James Troughton <jamesstroughton@gmail.com>
2022-01-11 11:49:14 +1300

    ✨ About Section

--- components/About.jsx
+++ components/About.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SectionHeader } from '.';
+import { gsap } from 'gsap/dist/gsap';
+import SplitText from '../utils/split-text.min.js';
+import useOnScreen from '../hooks/useOnScreen.js';
+import cn from 'classnames';
 
 const About = () => {
+  const ref = useRef(null);
+
+  const [reveal, setReveal] = useState(false);
+  const onScreen = useOnScreen(ref);
+
+  useEffect(() => {
+    if (onScreen) setReveal(onScreen);
+  }, [onScreen]);
+
+  useEffect(() => {
+    if (reveal) {
+      const split = new SplitText('#headline', {
+        type: 'lines',
+      });
+
+      gsap.to(split.lines, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        stagger: 0.1,
+        ease: 'power2',
+      });
+    }
+  }, [reveal]);
+
   return (
-    <section className='py-[100px]' id='about' data-scroll-section>
+    <section
+      className={cn('about-section py-[100px]', { 'is-reveal': reveal })}
+      id='about'
+      data-scroll-section
+    >
       <SectionHeader title='about' />
-      <p className='text-[4.5rem] leading-[1.12]' id='headline'>
+      <p
+        ref={ref}
+        className={cn('text-[4.5rem] leading-[1.12] opacity-0', {
+          'is-reveal': reveal,
+        })}
+        id='headline'
+      >
         I am a software engineer with skills in both front end and back end
         development of websites and web applications. As the next step in my
         career I am looking to work with a small team of passionate people to
