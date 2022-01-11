566bc5bd7c1701206f4a8054e6c1db8b3ad0190a
James Troughton <jamesstroughton@gmail.com>
2022-01-12 10:20:31 +1300

    💄 Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SectionHeader } from '.';
+import SplitText from '../utils/split-text.min.js';
+import { gsap } from 'gsap/dist/gsap';
+import useOnScreen from '../hooks/useOnScreen';
+import cn from 'classnames';
 
 const Footer = () => {
+  const ref = useRef(null);
+  const [reveal, setReveal] = useState(false);
+
+  const onScreen = useOnScreen(ref, 0.5);
+
+  useEffect(() => {
+    if (onScreen) {
+      setReveal(onScreen);
+    }
+  }, [onScreen]);
+
+  useEffect(() => {
+    if (reveal) {
+      const split = new SplitText('#location-text', {
+        type: 'lines',
+        linesClass: 'lineChildren',
+      });
+      const splitParent = new SplitText('#location-text', {
+        type: 'lines',
+        linesClass: 'lineParent',
+      });
+      gsap.fromTo(
+        split.lines,
+        { y: 200 },
+        {
+          duration: 1,
+          y: 0,
+          stagger: 0.1,
+          ease: 'power2',
+        }
+      );
+    }
+  }, [reveal]);
+
   return (
-    <section className='text-center pt-[100px] pb-[100px]' data-scroll-section>
+    <section
+      className='footer text-center pt-[100px] pb-[100px]'
+      data-scroll-section
+    >
       <SectionHeader title='Based in' />
 
       <h1
-        className='text-[14vw] lg:text-[11vw] 4xl:text-[10vw] uppercase leading-none font-["Bodoni_Moda"]'
+        className={cn(
+          'location text-[14vw] lg:text-[11vw] 4xl:text-[10vw] uppercase leading-none font-["Bodoni_Moda"]',
+          { 'is-reveal': reveal }
+        )}
         id='location-text'
+        ref={ref}
       >
         Auckland New Zealand
       </h1>
