19d22516bb5fe2081acbcfff8c8b82fee55176e2
James Troughton <jamesstroughton@gmail.com>
2022-02-12 22:30:16 +1300

    🎨 Removed Import

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // import Header from './Header';
 import CustomCursor from '../CustomCursor';
-import dynamic from 'next/dynamic';
 
 const Layout = ({ children }) => {
   return (
